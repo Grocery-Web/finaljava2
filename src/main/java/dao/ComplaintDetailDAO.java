4925fd6093e3ed98638f986fdd13ca071008212f
thangtm <thangkoit1k24@gmail.com>
2020-07-05 01:20:21 +0700

    add method to get people list

--- src/main/java/dao/ComplaintDetailDAO.java
+++ src/main/java/dao/ComplaintDetailDAO.java
@@ -3,12 +3,15 @@ package dao;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import common.ConnectToProperties;
 import entity.ComplaintDetail;
+import entity.Person;
+import dao.PersonDAO;
 
 public class ComplaintDetailDAO {
 	public List<Integer> findAllPersonByComplaintId(int id) {
@@ -27,6 +30,28 @@ public class ComplaintDetailDAO {
 		}
 
 		return personIdList;
+	}
+	
+	public HashMap<Person, String> getPeopleListByComplaintId(int id) {
+		HashMap<Person, String> personMap = new HashMap<Person, String>();
+		
+
+		try (var connect = DriverManager.getConnection(ConnectToProperties.getConnection());
+				PreparedStatement ps = connect.prepareCall("{call getComplaintDetailByComplaintId(?)}");) {
+			ps.setInt(1, id);
+
+			var rs = ps.executeQuery();
+			while (rs.next()) {
+				PersonDAO personDAO = new PersonDAO();
+				int personId = rs.getInt("personId");
+				String crimeType = rs.getString("crimeType");
+				Person person = PersonDAO.findPersonById(personId);
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+
+		return personMap;
 	}
 
 	public void setComplaintDetail(ComplaintDetail comDetail) {
