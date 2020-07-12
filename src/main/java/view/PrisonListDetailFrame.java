8cea3ce23889b12a6fa52221616a9735ea06292f
kevin <dqtkien@gmail.com>
2020-07-12 22:46:37 +0700

    add save img

--- src/main/java/view/PrisonListDetailFrame.java
+++ src/main/java/view/PrisonListDetailFrame.java
@@ -6,17 +6,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 
 import entity.PrisonList;
 import entity.PrisonerInList;
 
+import javax.imageio.ImageIO;
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -25,8 +32,11 @@ import java.util.List;
 
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PrisonListDetailFrame extends JFrame {
 
@@ -43,6 +53,9 @@ public class PrisonListDetailFrame extends JFrame {
 	private JButton btnUploadIMG;
 	private JScrollPane scrollPane;
 	private JTable table;
+	File imgChooser;
+	private JButton btnSave;
+	private String imgName;
 
 	/**
 	 * Launch the application.
@@ -73,6 +86,11 @@ public class PrisonListDetailFrame extends JFrame {
 		txtAddress.setText(pr.getAddress());
 		txtCapacity.setText(Integer.toString(pr.getCapacity()));
 		txtQuantity.setText(Integer.toString(pr.getQuantity()));
+		imgName = pr.getImg();
+	    
+		
+		//Display Prison's img
+		
 		try {
 			String url = "images/" + pr.getImg();
 			Image prisonIMG = 
@@ -161,15 +179,26 @@ public class PrisonListDetailFrame extends JFrame {
 		lblImg = new JLabel("");
 		
 		btnUploadIMG = new JButton("Update Image");
+		btnUploadIMG.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnUploadIMGactionPerformed(e);
+			}
+		});
 		
 		scrollPane = new JScrollPane();
+		
+		btnSave = new JButton("Save");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnSaveactionPerformed(e);
+			}
+		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(25)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 777, GroupLayout.PREFERRED_SIZE)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
 								.addComponent(lblCapacity, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -185,7 +214,10 @@ public class PrisonListDetailFrame extends JFrame {
 							.addGap(18)
 							.addComponent(lblImg, GroupLayout.PREFERRED_SIZE, 380, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
-							.addComponent(btnUploadIMG)))
+							.addComponent(btnUploadIMG))
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+							.addComponent(btnSave, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
+							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 777, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap(26, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
@@ -218,12 +250,46 @@ public class PrisonListDetailFrame extends JFrame {
 							.addGap(18)))
 					.addGap(18)
 					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 273, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(143, Short.MAX_VALUE))
+					.addGap(28)
+					.addComponent(btnSave)
+					.addContainerGap(76, Short.MAX_VALUE))
 		);
 		
 		table = new JTable();
 		table.setAutoCreateRowSorter(true);
 		scrollPane.setViewportView(table);
 		contentPane.setLayout(gl_contentPane);
+	}
+	protected void btnUploadIMGactionPerformed(ActionEvent e) {
+		
+		//Update images
+		JFileChooser filechooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images (jpg, gif, png)", "jpg","gif","png");
+		filechooser.setFileFilter(filter);
+		filechooser.setDialogTitle("Choose Another Prison's Image");
+		int returnval = filechooser.showOpenDialog(this);
+		if (returnval == JFileChooser.APPROVE_OPTION) {
+			imgChooser = filechooser.getSelectedFile();
+			BufferedImage bi;
+			try {
+				bi = ImageIO.read(imgChooser);
+			    lblImg.setIcon(new ImageIcon(bi.getScaledInstance(400, 130,  Image.SCALE_SMOOTH)));
+			} catch (Exception e2) {
+				JOptionPane.showMessageDialog(null, "Something went wrong. Please try again");
+			}
+		}
+	}
+	protected void btnSaveactionPerformed(ActionEvent e) {
+		if (imgChooser != null) {
+			try {
+				BufferedImage img = ImageIO.read(imgChooser);
+				String location = System.getProperty("user.dir") + "/src/main/resources/images/" + imgName;
+				String format = "PNG";
+				ImageIO.write(img, format, new File(location));
+				JOptionPane.showMessageDialog(null, "Successfully save image");
+			} catch (IOException e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "info", JOptionPane.ERROR_MESSAGE);
+			}
+		}
 	}
 }
