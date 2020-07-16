55affd63d657b3542f39cefe57ef87f35616fe96
thangtm <thangkoit1k24@gmail.com>
2020-07-17 00:29:36 +0700

    add validation incident detail

--- src/main/java/view/IncidentDetailFrame.java
+++ src/main/java/view/IncidentDetailFrame.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -14,6 +16,11 @@ import java.util.regex.Pattern;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableCellRenderer;
 
 import com.toedter.calendar.JDateChooser;
@@ -38,6 +45,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class IncidentDetailFrame extends JFrame {
 
@@ -55,10 +64,15 @@ public class IncidentDetailFrame extends JFrame {
 	private IncidentDetailVictimTableModel victimTableModel;
 	private TableIncidentDetailListener tableListener;
 	private JScrollPane jpTable, jpDetail, jpVictimTable;
+	private String s = Character.toString("\u2713".toCharArray()[0]);
+	private JLabel q1, q2, q4, q5, q3, q6, q7;
+	private boolean cd1, cd2, cd3, cd4, cd5, cd6, cd7, enabledMouseListener;
 	public SimpleDateFormat sdf0 = new SimpleDateFormat("yyyy-MM-dd");
 	public SimpleDateFormat sdf1 = new SimpleDateFormat("HH:mm:ss");
 	public SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private JTable tableVictim;
+	private JButton btnUpdate;
+
 	/**
 	 * Launch the application.
 	 */
@@ -80,7 +94,7 @@ public class IncidentDetailFrame extends JFrame {
 	 */
 	public IncidentDetailFrame(Complaint inc) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 843, 737);
+		setBounds(100, 100, 868, 726);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -90,7 +104,29 @@ public class IncidentDetailFrame extends JFrame {
 		JLabel lblName = new JLabel("Incident Name:");	
 		textName = new JTextField();
 		textName.setColumns(10);
-		textName.setText(inc.getName());
+		SwingUtilities.invokeLater(new Runnable() {	
+			@Override
+			public void run() {
+				textName.setText(inc.getName());	
+			}
+		});
+		textName.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				cd1Check();
+				checkUnlock();
+			}
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				cd1Check();
+				checkUnlock();
+			}
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				cd1Check();
+				checkUnlock();
+			}
+		});
 
 //		INCIDENT ID
 		JLabel lblID = new JLabel("ID:");
@@ -105,6 +141,13 @@ public class IncidentDetailFrame extends JFrame {
 		editor = (JTextFieldDateEditor) textDate.getDateEditor();
 		textDate.setDateFormatString("yyyy-MM-dd");
 		textDate.setDate(inc.getDatetime());
+		textDate.addPropertyChangeListener(new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				cd2Check(); cd3Check();
+				checkUnlock();
+			}
+		});
 		
 		JLabel lblTime = new JLabel("Time:");
 		timeSpinner = new JSpinner(new SpinnerDateModel());
@@ -125,18 +168,69 @@ public class IncidentDetailFrame extends JFrame {
 				}
 			}
 		});
+		timeSpinner.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				cd3Check();
+				checkUnlock();
+			}
+		});
 		
 //		INCIDENT PLACE
 		JLabel lblPlace = new JLabel("Place:");		
 		textPlace = new JTextField();
 		textPlace.setColumns(10);
-		textPlace.setText(inc.getPlace());
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				textPlace.setText(inc.getPlace());
+			}
+		});
+		textPlace.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				cd4Check();
+				checkUnlock();
+			}
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				cd4Check();
+				checkUnlock();
+			}
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				cd4Check();
+				checkUnlock();
+			}
+		});
 
 //		INCIDENT DECLARANT NAME
 		JLabel lblDeclarantName = new JLabel("Declarant Name:");		
 		textDeclarantName = new JTextField();
 		textDeclarantName.setColumns(10);
-		textDeclarantName.setText(inc.getDeclarantName());
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				textDeclarantName.setText(inc.getDeclarantName());
+			}
+		});
+		textDeclarantName.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				cd5Check();
+				checkUnlock();
+			}
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				cd5Check();
+				checkUnlock();
+			}
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				cd5Check();
+				checkUnlock();
+			}
+		});
 
 //		INCIDENT DETAIL
 		JLabel lblDetail = new JLabel("Detail:");		
@@ -147,6 +241,23 @@ public class IncidentDetailFrame extends JFrame {
 			@Override
 			public void run() {
 				textDetail.setText(inc.getDetail());
+			}
+		});
+		textDetail.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				cd6Check();
+				checkUnlock();
+			}
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				cd6Check();
+				checkUnlock();
+			}
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				cd6Check();
+				checkUnlock();
 			}
 		});
 		
@@ -184,9 +295,15 @@ public class IncidentDetailFrame extends JFrame {
 			tableVictim.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
 		}
 		
+		q1 = new JLabel();
+		q2 = new JLabel();
+		q4 = new JLabel();
+		q5 = new JLabel();
+		q3 = new JLabel();
+		q6 = new JLabel();
 		
 // 		UPDATE BUTTON		
-		JButton btnUpdate = new JButton("Update");
+		btnUpdate = new JButton("Update");
 		btnUpdate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				btnUpdateActionPerformed(e, inc.getId());
@@ -194,7 +311,6 @@ public class IncidentDetailFrame extends JFrame {
 		});
 		btnUpdate.setFocusPainted(false);
 		btnUpdate.setBackground(Color.YELLOW);
-		
 
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
@@ -207,7 +323,7 @@ public class IncidentDetailFrame extends JFrame {
 							.addComponent(lblVictimTable)
 							.addContainerGap())
 						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-							.addComponent(lblDeclarantName, GroupLayout.DEFAULT_SIZE, 814, Short.MAX_VALUE)
+							.addComponent(lblDeclarantName, GroupLayout.DEFAULT_SIZE, 810, Short.MAX_VALUE)
 							.addGroup(gl_contentPane.createSequentialGroup()
 								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 									.addComponent(lblName)
@@ -223,24 +339,36 @@ public class IncidentDetailFrame extends JFrame {
 										.addContainerGap())
 									.addGroup(gl_contentPane.createSequentialGroup()
 										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-											.addComponent(textDeclarantName, GroupLayout.DEFAULT_SIZE, 271, Short.MAX_VALUE)
+											.addComponent(textDeclarantName, GroupLayout.DEFAULT_SIZE, 413, Short.MAX_VALUE)
 											.addGroup(gl_contentPane.createSequentialGroup()
 												.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
 													.addComponent(textPlace, Alignment.LEADING)
 													.addComponent(textDate, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 													.addComponent(textName, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 125, Short.MAX_VALUE))
-												.addGap(32)
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+													.addComponent(q1, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
+													.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+														.addComponent(q4, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+														.addComponent(q2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)))
+												.addPreferredGap(ComponentPlacement.RELATED, 103, Short.MAX_VALUE)
 												.addComponent(lblTime)
-												.addPreferredGap(ComponentPlacement.RELATED)
-												.addComponent(timeSpinner, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)))
-										.addGap(413))
+												.addGap(18)
+												.addComponent(timeSpinner, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addComponent(q3, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)))
+										.addGap(10)
+										.addComponent(q5, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
+										.addGap(238))
 									.addGroup(gl_contentPane.createSequentialGroup()
 										.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-											.addComponent(jpVictimTable, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 											.addComponent(jpDetail, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 617, Short.MAX_VALUE)
 											.addComponent(jpTable, Alignment.LEADING)
+											.addComponent(jpVictimTable, Alignment.LEADING)
 											.addComponent(btnUpdate, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE))
-										.addContainerGap(38, Short.MAX_VALUE)))))))
+										.addPreferredGap(ComponentPlacement.UNRELATED)
+										.addComponent(q6, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+										.addContainerGap()))))))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -250,41 +378,54 @@ public class IncidentDetailFrame extends JFrame {
 						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 							.addComponent(lblTime)
 							.addComponent(timeSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(q3, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblName)
-								.addComponent(textName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(q1, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+									.addComponent(lblName)
+									.addComponent(textName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
 							.addGap(11)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(lblID)
-								.addComponent(textId, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(lblIDate)
-								.addComponent(textDate, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addComponent(lblID)
+										.addComponent(textId, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addComponent(lblIDate)
+										.addComponent(textDate, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+								.addComponent(q2, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))))
 					.addGap(18)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblPlace)
-						.addComponent(textPlace, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(textPlace, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(q4, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblDeclarantName)
-						.addComponent(textDeclarantName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
+						.addComponent(textDeclarantName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(q5, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblDetail)
-						.addComponent(jpDetail, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(18)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblDetail)
+								.addComponent(jpDetail, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblCriminalTable)
+								.addComponent(jpTable, GroupLayout.PREFERRED_SIZE, 141, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblVictimTable)
+								.addComponent(jpVictimTable, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(57)
+							.addComponent(q6, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)))
 					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblCriminalTable)
-						.addComponent(jpTable, GroupLayout.PREFERRED_SIZE, 141, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblVictimTable)
-						.addComponent(jpVictimTable, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE))
-					.addGap(29)
 					.addComponent(btnUpdate, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
-					.addGap(101))
+					.addGap(29))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
@@ -326,5 +467,109 @@ public class IncidentDetailFrame extends JFrame {
 				getCompDetail, true);
 
 		tableListener.tableEventUpdated(incident);
+	}
+	
+//  FUNCTIONS TO VALIDATE INPUT
+	private void cd1Check() {
+		if (!textName.getText().equals("") && textName.getText().matches("(\\d|[a-zA-Z]|\\s){3,50}")) {
+			textName.setBorder(new LineBorder(Color.GREEN, 1));
+			q1.setText(s); q1.setForeground(new Color(0, 153, 51));
+	       	q1.setToolTipText(null);
+	       	cd1 = true;
+		} else {
+			textName.setBorder(new LineBorder(Color.RED, 1));
+			q1.setText("?"); q1.setForeground(Color.RED);
+	       	q1.setToolTipText("3 - 50 characters required (alphabetical characters, numbers and spaces).");
+	       	cd1 = false;
+		}
+	}
+	
+	private void cd2Check() {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(textDate.getDate());
+		LocalDate date1 = LocalDate.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DATE));
+		LocalDate date2 = LocalDate.now();
+		if (date1 != null) {
+			if (date1.compareTo(date2) <= 0) {			
+				q2.setText(s); q2.setForeground(new Color(0, 153, 51));
+				q2.setToolTipText(null); editor.setBorder(new LineBorder(Color.GREEN, 1));
+				cd2 = true;
+			} else {
+				q2.setText("?"); q2.setForeground(Color.RED); editor.setBorder(new LineBorder(Color.RED, 1));
+				q2.setToolTipText("Select a date that is no later than today.");
+				cd2 = false;
+			}
+		}
+	}
+	
+	private void cd3Check() {
+		Date date1 = null;
+		try {
+			if (textDate.getDate() != null) {
+				String d = sdf0.format(textDate.getDate());
+				String t = sdf1.format(timeSpinner.getValue());
+				date1 = sdf2.parse(d + " " + t);
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		Date date2 = new Date();
+		if (date1.compareTo(date2) <= 0) {
+			q3.setText(s); q3.setForeground(new Color(0, 153, 51)); timeSpinner.getEditor().setBorder(new LineBorder(Color.GREEN));
+			q3.setToolTipText(null); 
+			cd3 = true;
+		} else {
+			q3.setText("?"); q3.setForeground(Color.RED); timeSpinner.getEditor().setBorder(new LineBorder(Color.RED));
+			q3.setToolTipText("Select a time that is no later than current time.");
+			cd3 = false;
+		}
+	}
+	
+	private void cd4Check() {
+		if (!textPlace.getText().equals("") && textPlace.getText().matches("(.|\\s){2,4000}")) {
+			textPlace.setBorder(new LineBorder(Color.GREEN, 1));
+        	q4.setText(s); q4.setForeground(new Color(0, 153, 51));
+        	q4.setToolTipText(null);
+            cd4 = true;
+        } else {
+        	textPlace.setBorder(new LineBorder(Color.RED, 1));
+        	q4.setText("?"); q4.setForeground(Color.RED);
+        	q4.setToolTipText("Minimum length required is 2 characters");
+        	cd4 = false;
+        }
+	}
+	
+	private void cd5Check() {
+		if (!textDeclarantName.getText().equals("") && textDeclarantName.getText().matches("[\\D ]{1,50}")) {
+        	textDeclarantName.setBorder(new LineBorder(Color.GREEN, 1));
+        	q5.setText(s); q5.setForeground(new Color(0, 153, 51));
+        	q5.setToolTipText(null);
+            cd5 = true;
+        } else {
+        	textDeclarantName.setBorder(new LineBorder(Color.RED, 1));
+        	q5.setText("?"); q5.setForeground(Color.RED);
+        	q5.setToolTipText("1 - 50 alphabet and special characters");
+        	cd5 = false;
+        }
+	}
+	
+	private void cd6Check() {
+		if (!textDetail.getText().equals("") && textDetail.getText().matches("(.|\\s){20,4000}")) {
+			textDetail.setBorder(new LineBorder(Color.GREEN, 1));
+        	q6.setText(s); q6.setForeground(new Color(0, 153, 51));
+        	q6.setToolTipText(null);
+        	cd6 = true;
+        } else {
+        	textDetail.setBorder(new LineBorder(Color.RED, 1));
+        	q6.setText("?"); q6.setForeground(Color.RED);
+        	q6.setToolTipText("Minimum length required is 20 characters");
+        	cd6 = false;
+        }
+	}
+	
+	private void checkUnlock() {
+		boolean unlock = cd1 && cd2 && cd3 && cd4 && cd5 && cd6;
+		btnUpdate.setEnabled(unlock);
+
 	}
 }
